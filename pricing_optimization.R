af18c8cc7a417e99c2413bbf992cf8455d1cf3e0
Kimmy-Yang <85028478+Kimmy-Yang@users.noreply.github.com>
2021-12-11 23:12:59 +0200

    Delete pricing_optimization.R

--- pricing_optimization.R
+++
@@ -1,9 +0,0 @@
-# PRIMER ON OPTIMIZATION 
-
-# define functions
-quantity <- function(price,a=100,b=.21) return(a-b*price)
-revenue <- function(price=0) return(price*quantity(price))
-
-# optimize within given boundaries
-optimize(revenue,interval = c(1,300),maximum=TRUE)
-
